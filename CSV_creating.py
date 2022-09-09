c6dbec7e6737c7df1eecf3fddbe1d93b3a0e3a5b
Mer94 <98532786+Mer94@users.noreply.github.com>
2022-09-09 20:38:06 +0500

    Update CSV_creating.py

--- CSV_creating.py
+++ CSV_creating.py
@@ -1,4 +1,4 @@
-def creating ():
+git rm def creating ():
     file = 'Phonebook.csv'
     with open (file, 'w', encoding = 'utf-8') as data:
         data.write(f'Фамилия;Имя;Номер телефона;Описание\n')
